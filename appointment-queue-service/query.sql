447ac2c3d6fb1ba6ff65e990a83a06291230c75a
Bagus Bayu Kencana <v.bagus.kencana@adira.co.id>
2025-07-29 20:59:08 +0700

    query create table

---
+++ appointment-queue-service/query.sql
@@ -0,0 +1,32 @@
+CREATE TABLE appointments (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    user_full_name VARCHAR(100) NOT NULL,
+    doctor_id INTEGER NOT NULL,
+    doctor_name VARCHAR(100) NOT NULL,
+    doctor_specialization VARCHAR(100) NOT NULL,
+    appointment_at TIMESTAMP NOT NULL,
+    is_paid BOOLEAN DEFAULT FALSE,
+    status VARCHAR(20) NOT NULL, -- e.g., 'paid', 'unpaid', 'void'
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE queues (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    user_name VARCHAR(100) NOT NULL,
+    user_role VARCHAR(20) NOT NULL, -- admin/member
+    patient_id INTEGER,             -- nullable
+    patient_name VARCHAR(100),      -- nullable
+    doctor_id INTEGER NOT NULL,
+    doctor_name VARCHAR(100) NOT NULL,
+    doctor_specialization VARCHAR(100) NOT NULL,
+    appointment_id INTEGER,         -- nullable
+    type VARCHAR(20) NOT NULL,      -- online / offline
+    queue_number INTEGER NOT NULL,
+    start_from TIMESTAMP NOT NULL,
+    status VARCHAR(20) NOT NULL,    -- active / fail / done
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
